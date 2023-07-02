271ceba46f00c89d9670d2ef0c76bc714b14e93f
Sathvik patil <sathvikpatil67@gmail.com>
2023-07-02 22:16:00 +0530

    Clarifai model fix

--- controllers/image.js
+++ controllers/image.js
@@ -4,7 +4,10 @@ const app = new Clarifai.App({
     apiKey: 'YOUR_API_KEY_HERE'
 });
 const handleApiCall = (req, res) => {
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    app.models.predict({
+        id: "a403429f2ddf4b49b307e318f00e528b",
+        version: "34ce21a40cc24b6b96ffee54aabff139",
+    }, req.body.input)
         .then(data => {
             res.json(data);
         })
